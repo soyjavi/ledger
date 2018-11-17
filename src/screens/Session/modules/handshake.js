5cc1f22fe9dca7fca5d128756ac921e1b0a5906f
@soyjavi <javi.jimenez.villar@gmail.com>
2018-11-17 08:39:49 +0700

    Control timeout

--- src/screens/Session/modules/handshake.js
+++ src/screens/Session/modules/handshake.js
@@ -3,11 +3,14 @@ import { C } from '../../../common';
 const { SCREEN } = C;
 
 export default async (component, { pin, store, navigation }) => {
+  component.setState({ busy: true });
+
   const hash = await store.getHash(pin).catch(error => component.setState({ error }));
   if (hash) {
     await store.getProfile().catch(error => component.setState({ error }));
     await store.getTransactions().catch(error => component.setState({ error }));
-    if (hash) navigation.navigate(SCREEN.DASHBOARD);
+    navigation.navigate(SCREEN.DASHBOARD);
   }
-  component.setState({ busy: false });
+
+  component.setState({ busy: false, pin: hash ? pin : '' });
 };
