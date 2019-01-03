d1d0bcfd42358d1a5937ff1987da0902b312a433
@soyjavi <javi.jimenez.villar@gmail.com>
2019-01-03 21:11:02 +0700

    <App> doesn’t contain the provider

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
 import React from 'react';
 
-import { C, L10N, getLocationAsync } from './common';
-import { Provider, Consumer, ConsumerNavigation } from './context';
+import { C } from './common';
+import { Consumer, ConsumerNavigation } from './context';
 import { LayoutView, Snackbar } from './reactor/components';
 import {
   Session, Dashboard, Stats, Vault,
 } from './screens';
 
-const { SCREEN, LANGUAGE } = C;
+const { SCREEN } = C;
 const {
   SESSION, DASHBOARD, STATS, VAULT,
 } = SCREEN;
 
 export default props => (
-  <Provider dictionary={L10N} language={LANGUAGE} getLocationAsync={getLocationAsync}>
-    <ConsumerNavigation>
-      { ({ current, params, stack }) => (
-        <LayoutView>
-          <Session backward={current !== SESSION} visible={stack.includes(SESSION)} />
+  <ConsumerNavigation>
+    { ({ current, params, stack }) => (
+      <LayoutView>
+        <Session backward={current !== SESSION} visible={stack.includes(SESSION)} />
 
-          <Dashboard backward={current !== DASHBOARD} visible={stack.includes(DASHBOARD)} />
-          <Vault
-            {...props}
-            backward={current !== VAULT}
-            navigation={{ state: { params: params.Vault } }}
-            visible={stack.includes(VAULT)}
-          />
-          <Stats backward={current !== STATS} visible={stack.includes(STATS)} />
+        <Dashboard backward={current !== DASHBOARD} visible={stack.includes(DASHBOARD)} />
+        <Vault
+          {...props}
+          backward={current !== VAULT}
+          navigation={{ state: { params: params.Vault } }}
+          visible={stack.includes(VAULT)}
+        />
+        <Stats backward={current !== STATS} visible={stack.includes(STATS)} />
 
-          <Consumer>
-            { ({ l10n, store: { error, onError } }) => (
-              <Snackbar
-                caption={error}
-                button={l10n.CLOSE}
-                visible={error !== undefined}
-                onPress={() => onError(undefined)}
-              />
-            )}
-          </Consumer>
-        </LayoutView>
-      )}
-    </ConsumerNavigation>
-  </Provider>
+        <Consumer>
+          { ({ l10n, store: { error, onError } }) => (
+            <Snackbar
+              caption={error}
+              button={l10n.CLOSE}
+              visible={error !== undefined}
+              onPress={() => onError(undefined)}
+            />
+          )}
+        </Consumer>
+      </LayoutView>
+    )}
+  </ConsumerNavigation>
 );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 
-import { theme } from './common';
+import {
+  C, getLocationAsync, L10N, theme,
+} from './common';
+import { Provider } from './context';
 import { THEME } from './reactor/common';
 
 THEME.extend(theme);
+const { LANGUAGE } = C;
 const App = require('./App').default;
 
-AppRegistry.registerComponent('App', () => App);
+const BrowserApp = () => (
+  <Provider dictionary={L10N} language={LANGUAGE} getLocationAsync={getLocationAsync}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent('App', () => BrowserApp);
 AppRegistry.runApplication('App', {
   initialProps: {},
   rootTag: document.getElementById('root'),
