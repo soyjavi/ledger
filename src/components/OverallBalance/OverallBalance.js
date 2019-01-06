1d7a944cccf0185097ebef1d9421df98507cb23d
@soyjavi <javi.jimenez.villar@gmail.com>
2019-01-06 11:11:02 +0700

    New style for OverallBalance

--- src/components/OverallBalance/OverallBalance.js
+++ src/components/OverallBalance/OverallBalance.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import { iconExpenses, iconIncomes } from '../../assets';
 import { C } from '../../common';
 import { Consumer } from '../../context';
-import { Icon, Price, Text } from '../../reactor/components';
+import { THEME } from '../../reactor/common';
+import { Price, Text } from '../../reactor/components';
+import BulletPrice from '../BulletPrice';
 import styles from './OverallBalance.style';
 
 const { FIXED, SYMBOL } = C;
+const { COLOR } = THEME;
 
 export default () => (
   <Consumer>
@@ -15,38 +17,24 @@ export default () => (
       <View style={styles.container}>
         <Text lighten subtitle level={2}>{l10n.OVERALL_BALANCE}</Text>
         <Price headline level={4} fixed={FIXED[baseCurrency]} value={overall.total} symbol={SYMBOL[baseCurrency]} />
-        <View style={styles.content}>
-          <View style={styles.context}>
-            <View style={styles.image}>
-              <Icon value={iconIncomes} />
-            </View>
-            <View>
-              <Text caption lighten>{l10n.INCOMES}</Text>
-              <Price
-                headline
-                level={6}
-                title="+"
-                fixed={FIXED[baseCurrency]}
-                value={overall.incomes}
-                symbol={SYMBOL[baseCurrency]}
-              />
-            </View>
-          </View>
-          <View style={styles.context}>
-            <View style={styles.image}>
-              <Icon value={iconExpenses} />
-            </View>
-            <View>
-              <Text caption lighten>{l10n.EXPENSES}</Text>
-              <Price
-                headline
-                level={6}
-                fixed={FIXED[baseCurrency]}
-                value={overall.expenses}
-                symbol={SYMBOL[baseCurrency]}
-              />
-            </View>
-          </View>
+        <Text subtitle level={3} lighten style={styles.month}>
+          {l10n.MONTHS[new Date().getMonth()]}
+        </Text>
+        <View style={styles.row}>
+          <BulletPrice
+            color={COLOR.TEXT}
+            currency={baseCurrency}
+            incomes
+            value={overall.incomes}
+            style={styles.bulletPrice}
+          />
+          <BulletPrice
+            color={COLOR.TEXT}
+            currency={baseCurrency}
+            incomes
+            value={overall.expenses}
+            style={styles.bulletPrice}
+          />
         </View>
       </View>
     )}
