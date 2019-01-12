39c862daaac8b5ca23bca4138ec2a13c0b916c77
@soyjavi <javi.jimenez.villar@gmail.com>
2019-01-12 16:15:07 +0700

    Improved <Chart> bars height

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,31 +1,33 @@
-import { arrayOf, number, string } from 'prop-types';
+import {
+  arrayOf, bool, number, string,
+} from 'prop-types';
 import React from 'react';
 import { View } from 'react-native';
 
 import { THEME } from '../../reactor/common';
-import { Text, Motion } from '../../reactor/components';
+import { Text } from '../../reactor/components';
 import styles from './Chart.style';
 
 const { COLOR } = THEME;
 
 const Chart = ({
-  color, inheritValue, maxValue, title, values,
+  color, inheritValue, title, values,
 }) => {
-  let max = maxValue;
-  if (!max) max = values.length > 0 ? Math.max(...values) : 0;
+  const max = values.length > 0 ? Math.max(...values) : 0;
+  const min = values.length > 0 ? Math.min(...values) : 0;
   const style = [styles.bar, values.length > 12 && styles.barTiny];
 
   return (
-    <View>
-      { title && <Text subtitle level={3} lighten>{title}</Text> }
-      <View style={styles.container}>
+    <View style={styles.container}>
+      { title && <Text subtitle level={3} lighten style={styles.title}>{title}</Text> }
+      <View style={styles.chart}>
         { values.map((value, index) => (
-          <Motion
+          <View
             key={`${index}-${value}`} // eslint-disable-line
             style={[
               ...style,
               {
-                height: `${parseInt((value * 100) / max, 10)}%`,
+                height: `${parseInt(((value - min) * 100) / (max - min), 10)}%`,
                 opacity: inheritValue === value ? 0.2 : 1,
               },
               color && { backgroundColor: color },
@@ -39,7 +41,6 @@ const Chart = ({
 Chart.propTypes = {
   color: string,
   inheritValue: number,
-  maxValue: number,
   title: string,
   values: arrayOf(number),
 };
@@ -47,7 +48,6 @@ Chart.propTypes = {
 Chart.defaultProps = {
   color: COLOR.PRIMARY,
   inheritValue: undefined,
-  maxValue: undefined,
   title: undefined,
   values: [],
 };
--- src/components/Chart/Chart.style.js
+++ src/components/Chart/Chart.style.js
@@ -3,29 +3,40 @@ import { StyleSheet } from 'react-native';
 import { LAYOUT, THEME } from '../../reactor/common';
 
 const { COLOR, UNIT } = THEME;
-const BAR_SIZE = UNIT * 0.2;
+const BAR_SIZE = UNIT * 0.8;
 
 export default StyleSheet.create({
-  container: {
-    ...LAYOUT.STYLE.ROW,
-    alignItems: 'flex-end',
-    height: UNIT * 4.4,
-    justifyContent: 'space-between',
-  },
-
   bar: {
     backgroundColor: COLOR.BASE,
     borderRadius: BAR_SIZE,
     height: '100%',
-    marginHorizontal: BAR_SIZE,
-    minHeight: BAR_SIZE * 2,
-    width: BAR_SIZE * 2,
+    marginLeft: UNIT * 0.1,
+    minHeight: BAR_SIZE,
+    width: BAR_SIZE,
   },
 
   barTiny: {
-    borderRadius: 0,
-    marginHorizontal: BAR_SIZE / 2,
-    minHeight: BAR_SIZE,
-    width: BAR_SIZE,
+    borderRadius: BAR_SIZE / 2,
+    minHeight: BAR_SIZE / 2,
+    width: BAR_SIZE / 2,
+  },
+
+  chart: {
+    ...LAYOUT.STYLE.ROW,
+    alignItems: 'flex-end',
+    alignContent: 'flex-end',
+    justifyContent: 'flex-end',
+    flex: 1,
+  },
+
+  container: {
+    alignSelf: 'flex-end',
+    height: UNIT * 7.2,
+    width: UNIT * 16.4,
+  },
+
+  title: {
+    textAlign: 'right',
+    marginBottom: UNIT / 4,
   },
 });
