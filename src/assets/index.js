212b9af6cba3daff04035782cfd75d77191192f0
@soyjavi <javi.jimenez.villar@gmail.com>
2019-01-30 13:36:26 +0700

    New icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,19 @@
 import iconAdd from './icon_add.png';
 import iconBack from './icon_back.png';
 import iconChart from './icon_chart.png';
+import iconCategory1 from './icon_category_1.png';
+import iconCategory2 from './icon_category_2.png';
+import iconCategory3 from './icon_category_3.png';
+import iconCategory4 from './icon_category_4.png';
+import iconCategory5 from './icon_category_5.png';
+import iconCategory6 from './icon_category_6.png';
+import iconCategory7 from './icon_category_7.png';
+import iconCategory8 from './icon_category_8.png';
+import iconCategory9 from './icon_category_9.png';
+import iconCategory10 from './icon_category_10.png';
+import iconCategory11 from './icon_category_11.png';
+import iconCategory12 from './icon_category_12.png';
+import iconCategory99 from './icon_category_12.png';
 import iconExpenses from './icon_expenses.png';
 import iconFingerprint from './icon_fingerprint.png';
 import iconIncomes from './icon_incomes.png';
@@ -14,6 +27,19 @@ export {
   iconAdd,
   iconBack,
   iconChart,
+  iconCategory1,
+  iconCategory2,
+  iconCategory3,
+  iconCategory4,
+  iconCategory5,
+  iconCategory6,
+  iconCategory7,
+  iconCategory8,
+  iconCategory9,
+  iconCategory10,
+  iconCategory11,
+  iconCategory12,
+  iconCategory99,
   iconExpenses,
   iconFingerprint,
   iconIncomes,
@@ -22,4 +48,20 @@ export {
   iconTime,
 
   logo,
+};
+
+export default {
+  iconCategory1,
+  iconCategory2,
+  iconCategory3,
+  iconCategory4,
+  iconCategory5,
+  iconCategory6,
+  iconCategory7,
+  iconCategory8,
+  iconCategory9,
+  iconCategory10,
+  iconCategory11,
+  iconCategory12,
+  iconCategory99,
 };
