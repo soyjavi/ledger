d1e09323e60c0fd6c35797bd7e5536ceee9181b5
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-01 08:44:32 +0700

    Refactor icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,67 +1,68 @@
 import iconAdd from './icon_add.png';
 import iconBack from './icon_back.png';
 import iconChart from './icon_chart.png';
-import iconCategory1 from './icon_category_1.png';
-import iconCategory2 from './icon_category_2.png';
-import iconCategory3 from './icon_category_3.png';
-import iconCategory4 from './icon_category_4.png';
-import iconCategory5 from './icon_category_5.png';
-import iconCategory6 from './icon_category_6.png';
-import iconCategory7 from './icon_category_7.png';
-import iconCategory8 from './icon_category_8.png';
-import iconCategory9 from './icon_category_9.png';
-import iconCategory10 from './icon_category_10.png';
-import iconCategory11 from './icon_category_11.png';
-import iconCategory12 from './icon_category_12.png';
-import iconCategory99 from './icon_category_12.png';
-import iconExpenses from './icon_expenses.png';
+import iconType0Category1 from './icon_expense_1.png';
+import iconType0Category2 from './icon_expense_2.png';
+import iconType0Category3 from './icon_expense_3.png';
+import iconType0Category4 from './icon_expense_4.png';
+import iconType0Category5 from './icon_expense_5.png';
+import iconType0Category6 from './icon_expense_6.png';
+import iconType0Category7 from './icon_expense_7.png';
+import iconType0Category8 from './icon_expense_8.png';
+import iconType0Category9 from './icon_expense_9.png';
+import iconType0Category10 from './icon_expense_10.png';
+import iconType0Category11 from './icon_expense_11.png';
+import iconType0Category12 from './icon_expense_12.png';
+import iconType0Categorys from './icon_expenses.png';
 import iconFingerprint from './icon_fingerprint.png';
+import iconType1Category1 from './icon_income_1.png';
+import iconType1Category2 from './icon_income_2.png';
+import iconType1Category3 from './icon_income_3.png';
+import iconType1Category4 from './icon_income_4.png';
 import iconIncomes from './icon_incomes.png';
 import iconPlace from './icon_place.png';
 import iconShuffle from './icon_shuffle.png';
 import iconTime from './icon_time.png';
-
+import iconVaultTransfer from './icon_vault_transfer.png';
 import logo from './logo.png';
 
-export {
+
+export default {
   iconAdd,
+
   iconBack,
+
   iconChart,
-  iconCategory1,
-  iconCategory2,
-  iconCategory3,
-  iconCategory4,
-  iconCategory5,
-  iconCategory6,
-  iconCategory7,
-  iconCategory8,
-  iconCategory9,
-  iconCategory10,
-  iconCategory11,
-  iconCategory12,
-  iconCategory99,
-  iconExpenses,
+
+  iconType0Category1,
+  iconType0Category2,
+  iconType0Category3,
+  iconType0Category4,
+  iconType0Category5,
+  iconType0Category6,
+  iconType0Category7,
+  iconType0Category8,
+  iconType0Category9,
+  iconType0Category10,
+  iconType0Category11,
+  iconType0Category12,
+  iconType0Category99: iconVaultTransfer,
+  iconType0Categorys,
+
   iconFingerprint,
+
+  iconType1Category1,
+  iconType1Category2,
+  iconType1Category3,
+  iconType1Category4,
   iconIncomes,
+  iconIncome99: iconVaultTransfer,
+
   iconPlace,
+
   iconShuffle,
+
   iconTime,
 
   logo,
-};
-
-export default {
-  iconCategory1,
-  iconCategory2,
-  iconCategory3,
-  iconCategory4,
-  iconCategory5,
-  iconCategory6,
-  iconCategory7,
-  iconCategory8,
-  iconCategory9,
-  iconCategory10,
-  iconCategory11,
-  iconCategory12,
-  iconCategory99,
 };
