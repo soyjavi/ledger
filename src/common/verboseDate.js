ec5241ee0f21be414e50ab6e73de74ebde77a345
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-03 05:04:42 +0700

    Improver verbose date

--- src/common/verboseDate.js
+++ src/common/verboseDate.js
@@ -2,11 +2,11 @@ export default (timestamp, l10n) => {
   const today = new Date();
   const date = new Date(timestamp);
 
-  let verboseDay = '';
+  let verboseDay;
   if (date.getMonth() === today.getMonth()) {
     if (date.getDate() === today.getDate()) verboseDay = l10n.TODAY;
     if (date.getDate() === (today.getDate() - 1)) verboseDay = l10n.YESTERDAY; // @TODO How about if change month
   }
 
-  return `${verboseDay ? `${verboseDay}, ` : ''}${l10n.MONTHS[date.getMonth()]}`;
+  return verboseDay || `${date.getDate()} ${l10n.MONTHS[date.getMonth()]}`;
 };
