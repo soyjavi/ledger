98d21c63f13086dbcac25bfb46dc19f5157f3e23
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-02 13:31:27 +0700

    Refactor icons (again)

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,11 @@
 import iconAdd from './icon_add.png';
 import iconBack from './icon_back.png';
 import iconChart from './icon_chart.png';
+import iconExpense from './icon_expense.png';
+import iconExpenseTrend from './icon_expense_trend.png';
+import iconFingerprint from './icon_fingerprint.png';
+import iconIncome from './icon_income.png';
+import iconIncomeTrend from './icon_income_trend.png';
 import iconType0Category1 from './icon_expense_1.png';
 import iconType0Category2 from './icon_expense_2.png';
 import iconType0Category3 from './icon_expense_3.png';
@@ -13,13 +18,10 @@ import iconType0Category9 from './icon_expense_9.png';
 import iconType0Category10 from './icon_expense_10.png';
 import iconType0Category11 from './icon_expense_11.png';
 import iconType0Category12 from './icon_expense_12.png';
-import iconType0Categorys from './icon_expenses.png';
-import iconFingerprint from './icon_fingerprint.png';
 import iconType1Category1 from './icon_income_1.png';
 import iconType1Category2 from './icon_income_2.png';
 import iconType1Category3 from './icon_income_3.png';
 import iconType1Category4 from './icon_income_4.png';
-import iconIncomes from './icon_incomes.png';
 import iconPlace from './icon_place.png';
 import iconShuffle from './icon_shuffle.png';
 import iconTime from './icon_time.png';
@@ -34,6 +36,14 @@ export default {
 
   iconChart,
 
+  iconExpense,
+  iconExpenseTrend,
+
+  iconFingerprint,
+
+  iconIncome,
+  iconIncomeTrend,
+
   iconType0Category1,
   iconType0Category2,
   iconType0Category3,
@@ -47,16 +57,11 @@ export default {
   iconType0Category11,
   iconType0Category12,
   iconType0Category99: iconVaultTransfer,
-  iconType0Categorys,
-
-  iconFingerprint,
-
   iconType1Category1,
   iconType1Category2,
   iconType1Category3,
   iconType1Category4,
   iconType1Category99: iconVaultTransfer,
-  iconIncomes,
 
   iconPlace,
 
