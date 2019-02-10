82ea473c0c59eade2d381ade5e9452bbd528ff01
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-10 15:27:51 +0700

    Remove unnecessary icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,74 +2,16 @@ import iconAdd from './icon_add.png';
 import iconBack from './icon_back.png';
 import iconChart from './icon_chart.png';
 import iconExpense from './icon_expense.png';
-import iconExpenseTrend from './icon_expense_trend.png';
 import iconFingerprint from './icon_fingerprint.png';
 import iconIncome from './icon_income.png';
-import iconIncomeTrend from './icon_income_trend.png';
-import iconType0Category1 from './icon_expense_1.png';
-import iconType0Category2 from './icon_expense_2.png';
-import iconType0Category3 from './icon_expense_3.png';
-import iconType0Category4 from './icon_expense_4.png';
-import iconType0Category5 from './icon_expense_5.png';
-import iconType0Category6 from './icon_expense_6.png';
-import iconType0Category7 from './icon_expense_7.png';
-import iconType0Category8 from './icon_expense_8.png';
-import iconType0Category9 from './icon_expense_9.png';
-import iconType0Category10 from './icon_expense_10.png';
-import iconType0Category11 from './icon_expense_11.png';
-import iconType0Category12 from './icon_expense_12.png';
-import iconType0Category13 from './icon_expense_13.png';
-import iconType1Category1 from './icon_income_1.png';
-import iconType1Category2 from './icon_income_2.png';
-import iconType1Category3 from './icon_income_3.png';
-import iconType1Category4 from './icon_income_4.png';
-import iconPlace from './icon_place.png';
-import iconShuffle from './icon_shuffle.png';
-import iconTime from './icon_time.png';
-import iconVaultTransfer from './icon_vault_transfer.png';
 import logo from './logo.png';
-
 
 export default {
   iconAdd,
-
   iconBack,
-
   iconChart,
-
   iconExpense,
-  iconExpenseTrend,
-
   iconFingerprint,
-
   iconIncome,
-  iconIncomeTrend,
-
-  iconType0Category1,
-  iconType0Category2,
-  iconType0Category3,
-  iconType0Category4,
-  iconType0Category5,
-  iconType0Category6,
-  iconType0Category7,
-  iconType0Category8,
-  iconType0Category9,
-  iconType0Category10,
-  iconType0Category11,
-  iconType0Category12,
-  iconType0Category13,
-  iconType0Category99: iconVaultTransfer,
-  iconType1Category1,
-  iconType1Category2,
-  iconType1Category3,
-  iconType1Category4,
-  iconType1Category99: iconVaultTransfer,
-
-  iconPlace,
-
-  iconShuffle,
-
-  iconTime,
-
   logo,
 };
