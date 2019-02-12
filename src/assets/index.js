0c78e3a3a3a94e66538a38c34e792df7d501e7c2
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-12 19:02:07 +0700

    New assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 import iconAdd from './icon_add.png';
 import iconBack from './icon_back.png';
-import iconChart from './icon_chart.png';
 import iconExpense from './icon_expense.png';
 import iconFingerprint from './icon_fingerprint.png';
 import iconIncome from './icon_income.png';
@@ -9,7 +8,6 @@ import logo from './logo.png';
 export default {
   iconAdd,
   iconBack,
-  iconChart,
   iconExpense,
   iconFingerprint,
   iconIncome,
