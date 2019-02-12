bd82c53f10ff05eb64b1b3430278548fcd2c3308
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-12 19:02:59 +0700

    Improved <Percentage>

--- src/components/Percentage/Percentage.js
+++ src/components/Percentage/Percentage.js
@@ -5,12 +5,13 @@ import { View } from 'react-native';
 import ASSETS from '../../assets';
 import { LAYOUT, THEME } from '../../reactor/common';
 import { Icon, Price } from '../../reactor/components';
+import styles from './Percentage.style';
 
 const { COLOR } = THEME;
 
 const PriceFriendly = ({ currency, value, ...inherit }) => (
   <View style={LAYOUT.STYLE.ROW}>
-    <Icon value={value > 0 ? ASSETS.iconIncome : ASSETS.iconExpense} />
+    <Icon value={value > 0 ? ASSETS.iconIncome : ASSETS.iconExpense} style={styles.icon} />
     <Price
       fixed={Math.abs(value) < 1 ? 2 : 0}
       color={value > 0 ? COLOR.INCOMES : COLOR.EXPENSES}
