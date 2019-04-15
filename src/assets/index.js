937fc55d3350954ccdab634a5309cb16a5ac4122
@soyjavi <javi.jimenez.villar@gmail.com>
2019-04-15 19:38:49 +0700

    New icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,7 @@
-import iconAdd from './icon_add.png';
-import iconBack from './icon_back.png';
-import expense from './icon_expense.png';
-import iconFingerprint from './icon_fingerprint.png';
-import income from './icon_income.png';
-import logo from './logo.png';
+import investment from './category-investment.png';
+import others from './category-others.png';
+import transfer from './category-transfer.png';
+import wipe from './category-wipe.png';
 
 import AUD from './flag-australia.png';
 import BTC from './flag-bitcoin.png';
@@ -17,17 +15,52 @@ import GBP from './flag-united-kingdom.png';
 import USD from './flag-united-states.png';
 import VND from './flag-vietnam.png';
 
+import add from './icon_add.png';
+import back from './icon_back.png';
+import expense from './icon_expense.png';
+import fingerprint from './icon_fingerprint.png';
+import income from './icon_income.png';
+import logo from './logo.png';
+
+
+const CATEGORIES = [
+  {
+    0: wipe,
+    1: require('./category-food-and-drinks.png'),
+    3: require('./category-travel.png'),
+    4: require('./category-debt.png'),
+    5: investment,
+    6: require('./category-entertainment.png'),
+    7: require('./category-shopping.png'),
+    8: require('./category-utilities.png'),
+    9: require('./category-healthcare.png'),
+    10: require('./category-personal.png'),
+    11: require('./category-services.png'),
+    12: transfer,
+    13: others,
+  },
+  {
+    0: wipe,
+    1: require('./category-salary.png'),
+    2: investment,
+    3: require('./category-pasives.png'),
+    4: transfer,
+    5: others,
+  },
+];
+
 const FLAGS = {
   AUD, BTC, EUR, HKD, JPY, KRW, MYR, THB, GBP, USD, VND,
 };
 
-export { FLAGS };
+export { CATEGORIES, FLAGS };
 
 export default {
-  iconAdd,
-  iconBack,
+  add,
+  back,
   expense,
-  iconFingerprint,
+  fingerprint,
   income,
   logo,
+  transfer,
 };
