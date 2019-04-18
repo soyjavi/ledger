8edd05cb84dfaf0826e9fa8f777af8b35319ab2e
@soyjavi <javi.jimenez.villar@gmail.com>
2019-04-18 11:36:12 +0700

    Don’t show WIPEs or VAULT_TRANSFERs

--- src/screens/Dashboard/modules/queryLastTxs.js
+++ src/screens/Dashboard/modules/queryLastTxs.js
@@ -1,5 +1,10 @@
+import { C } from '../../../common';
+
+const { VAULT_TRANSFER, WIPE } = C;
+
 export default ({ txs, vaults }) => txs
   .slice(-10)
+  .filter(({ category }) => category !== VAULT_TRANSFER && category !== WIPE)
   .map((tx) => {
     const { currency } = vaults.find(vault => vault.hash === tx.vault);
     return { currency, ...tx };
