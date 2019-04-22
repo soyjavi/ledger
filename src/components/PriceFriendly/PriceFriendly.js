813aef46e760d507339d0902fa04776a81ef8506
@soyjavi <javi.jimenez.villar@gmail.com>
2019-04-22 16:24:15 +0700

    First steps using a mask

--- src/components/PriceFriendly/PriceFriendly.js
+++ src/components/PriceFriendly/PriceFriendly.js
@@ -1,27 +1,35 @@
-import { number, string } from 'prop-types';
+import { bool, number, string } from 'prop-types';
 import React from 'react';
 
 import { C } from '../../common';
-import { Price } from '../../reactor/components';
+import { Price, Text } from '../../reactor/components';
 
 const { FIXED, SYMBOL } = C;
 
-const PriceFriendly = ({ currency, value, ...inherit }) => (
-  <Price
-    fixed={value < 1000 ? FIXED[currency] : 0}
-    symbol={SYMBOL[currency]}
-    value={Math.abs(value)}
-    {...inherit}
-  />
+const PriceFriendly = ({
+  currency, mask, value = 0, ...inherit
+}) => (
+  mask
+    ? <Text {...inherit}>{`${'?'.repeat(parseInt(value, 10).toString().length)} ${SYMBOL[currency]}`}</Text>
+    : (
+      <Price
+        fixed={value < 1000 ? FIXED[currency] : 0}
+        symbol={SYMBOL[currency]}
+        value={Math.abs(value)}
+        {...inherit}
+      />
+    )
 );
 
 PriceFriendly.propTypes = {
   currency: string,
+  mask: bool,
   value: number,
 };
 
 PriceFriendly.defaultProps = {
   currency: undefined,
+  mask: false,
   value: 0,
 };
 
