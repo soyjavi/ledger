4731ff804650fb85d77e7261289423a2b4549826
@soyjavi <javi.jimenez.villar@gmail.com>
2019-05-12 07:31:53 +0700

    Set prop `mask` within <Dashboard> & refacto spread

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Fragment } from 'react';
 import { StatusBar } from 'react-native';
 
 import { C } from './common';
-import {
-  Consumer, ConsumerEvents, ConsumerNavigation, ConsumerStore,
-} from './context';
+import { Consumer } from './context';
 import { THEME } from './reactor/common';
 import { LayoutView, Snackbar } from './reactor/components';
 import { DialogClone } from './components';
@@ -13,71 +11,56 @@ import {
 } from './screens';
 import styles from './App.style';
 
-const { SCREEN, SETTINGS: { NIGHT_MODE } } = C;
+const { SCREEN, SETTINGS: { HIDE_OVERALL_BALANCE, NIGHT_MODE } } = C;
 const {
   SESSION, SETTINGS, STATS, DASHBOARD, VAULT,
 } = SCREEN;
 const { COLOR } = THEME;
 
 export default () => (
-  <ConsumerNavigation>
+  <Consumer>
     { ({
-      current, goBack, params, stack,
+      events,
+      l10n,
+      navigation: {
+        current, goBack, params, stack,
+      },
+      store: {
+        error, onError, settings: { [HIDE_OVERALL_BALANCE]: mask, [NIGHT_MODE]: nightMode }, ...store
+      },
     }) => (
       <LayoutView style={styles.container}>
-        { stack.includes(SESSION) && (
-          <ConsumerStore>
-            { ({ settings }) => (
-              <StatusBar
-                backgroundColor={COLOR.BACKGROUND}
-                barStyle={settings[NIGHT_MODE] ? 'light-content' : 'dark-content'}
-              />
-            )}
-          </ConsumerStore>
-        )}
-
         { console.log('<App>') }
-        <ConsumerEvents>
-          { events => <Session {...events} backward={current !== SESSION} visible={stack.includes(SESSION)} /> }
-        </ConsumerEvents>
-        <Dashboard backward={current !== DASHBOARD} visible={stack.includes(DASHBOARD)} />
-        <Settings backward={current !== SETTINGS} visible={stack.includes(SETTINGS)} />
+        <Session {...events} backward={current !== SESSION} visible={stack.includes(SESSION)} />
+
+        { stack.includes(SESSION) && (
+          <Fragment>
+            <StatusBar
+              backgroundColor={COLOR.BACKGROUND}
+              barStyle={nightMode ? 'light-content' : 'dark-content'}
+            />
+            <Dashboard backward={current !== DASHBOARD} mask={mask} visible={stack.includes(DASHBOARD)} />
+            <Snackbar caption={error} button={l10n.CLOSE} visible={!!(error)} onPress={() => onError(undefined)} />
+          </Fragment>
+        )}
 
         { stack.includes(DASHBOARD) && (
-          <ConsumerStore>
-            { ({ settings, ...store }) => (
-              <Fragment>
-                <Vault
-                  backward={current !== VAULT}
-                  dataSource={stack.includes(VAULT) && params.Vault
-                    ? store.vaults.find(({ hash }) => hash === params.Vault.hash)
-                    : undefined
-                  }
-                  goBack={goBack}
-                  visible={stack.includes(VAULT)}
-                />
-                <Stats {...store} backward={current !== STATS} vault={params.Vault} visible={stack.includes(STATS)} />
-                <DialogClone
-                  dataSource={store.tx}
-                  highlight={settings[NIGHT_MODE]}
-                  visible={store.tx !== undefined}
-                />
-              </Fragment>
-            )}
-          </ConsumerStore>
-        )}
-
-        <Consumer>
-          { ({ l10n, store: { error, onError } }) => (
-            <Snackbar
-              caption={error}
-              button={l10n.CLOSE}
-              visible={error !== undefined}
-              onPress={() => onError(undefined)}
+          <Fragment>
+            <Settings backward={current !== SETTINGS} visible={stack.includes(SETTINGS)} />
+            <Vault
+              backward={current !== VAULT}
+              dataSource={stack.includes(VAULT) && params.Vault
+                ? store.vaults.find(({ hash }) => hash === params.Vault.hash)
+                : undefined
+              }
+              goBack={goBack}
+              visible={stack.includes(VAULT)}
             />
-          )}
-        </Consumer>
+            <Stats {...store} backward={current !== STATS} vault={params.Vault} visible={stack.includes(STATS)} />
+            <DialogClone dataSource={store.tx} highlight={nightMode} visible={store.tx !== undefined} />
+          </Fragment>
+        )}
       </LayoutView>
     )}
-  </ConsumerNavigation>
+  </Consumer>
 );
