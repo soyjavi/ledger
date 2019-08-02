1dd484c13a18757e4137a46cc730545d9d5e2d07
soyjavi <javi.jimenez.villar@gmail.com>
2019-08-02 09:22:56 +0700

    unnecesary file

--- src/screens/Settings/modules/query.js
+++
@@ -1,5 +0,0 @@
-export default (vaults = []) => vaults.sort((a, b) => {
-  if (a.currentBalanceBase < b.currentBalanceBase) return 1;
-  if (a.currentBalanceBase > b.currentBalanceBase) return -1;
-  return 0;
-});
