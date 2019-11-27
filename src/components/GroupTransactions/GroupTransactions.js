b8c970a2105604bd7036d98d3d8e6fe9f5d90e33
soyjavi <javi.jimenez.villar@gmail.com>
2019-11-27 15:36:11 +0700

    More react.memo's

--- src/components/GroupTransactions/GroupTransactions.js
+++ src/components/GroupTransactions/GroupTransactions.js
@@ -1,7 +1,7 @@
 import {
   arrayOf, number, shape, string,
 } from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { Text } from '../../reactor/components';
 
@@ -12,56 +12,43 @@ import PriceFriendly from '../PriceFriendly';
 import TransactionItem from '../TransactionItem';
 import styles from './GroupTransactions.style';
 
-class GroupTransactions extends Component {
-  static propTypes = {
-    currency: string.isRequired,
-    timestamp: string.isRequired,
-    txs: arrayOf(shape()).isRequired,
-    value: number,
-  };
-
-  static defaultProps = {
-    value: 0,
-  };
-
-  shouldComponentUpdate(nextProps) {
-    const { props: { timestamp, value } } = this;
-    return timestamp !== nextProps.timestamp && value !== nextProps.value;
-  }
-
-  render() {
-    const {
-      props: {
-        currency, timestamp, txs = [], value,
-      },
-    } = this;
-
-    return (
-      <Consumer>
-        { ({ store: { baseCurrency, rates }, l10n }) => (
-          <View style={styles.container}>
-            <View style={[styles.row, styles.heading]}>
-              <Box style={styles.tag}>
-                <Text subtitle level={3}>{verboseDate(timestamp, l10n)}</Text>
-              </Box>
-              { value !== 0 && (
-                <View style={styles.tag}>
-                  <PriceFriendly
-                    currency={baseCurrency}
-                    icon
-                    subtitle
-                    level={3}
-                    value={exchange(value, currency, baseCurrency, rates, timestamp)}
-                  />
-                </View>
-              )}
+const GroupTransactions = React.memo(({
+  currency, timestamp, txs = [], value,
+}) => (
+  <Consumer>
+    { ({ store: { baseCurrency, rates }, l10n }) => (
+      <View style={styles.container}>
+        <View style={[styles.row, styles.heading]}>
+          <Box style={styles.tag}>
+            <Text subtitle level={3}>{verboseDate(timestamp, l10n)}</Text>
+          </Box>
+          { value !== 0 && (
+            <View style={styles.tag}>
+              <PriceFriendly
+                currency={baseCurrency}
+                icon
+                subtitle
+                level={3}
+                value={exchange(value, currency, baseCurrency, rates, timestamp)}
+              />
             </View>
-            { txs.map((tx) => <TransactionItem key={tx.hash} currency={currency} {...tx} />)}
-          </View>
-        )}
-      </Consumer>
-    );
-  }
-}
+          )}
+        </View>
+        { txs.map((tx) => <TransactionItem key={tx.hash} currency={currency} {...tx} />)}
+      </View>
+    )}
+  </Consumer>
+));
+
+GroupTransactions.propTypes = {
+  currency: string.isRequired,
+  timestamp: string.isRequired,
+  txs: arrayOf(shape()).isRequired,
+  value: number,
+};
+
+GroupTransactions.defaultProps = {
+  value: 0,
+};
 
 export default GroupTransactions;
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,7 +1,7 @@
 import {
   bool, func, shape, number, oneOfType, string,
 } from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { Image, View } from 'react-native';
 
 import ASSETS from '../../assets';
@@ -27,110 +27,98 @@ PriceSmall.propTypes = {
   value: number.isRequired,
 };
 
-class Summary extends Component {
-  static propTypes = {
-    currency: string,
-    currentBalance: number,
-    currentMonth: shape({}),
-    image: oneOfType([number, string]),
-    mask: bool,
-    onMask: func,
-    title: string.isRequired,
-  };
+const Summary = React.memo(({
+  currency, currentBalance, currentMonth, image, mask, onMask, title, ...inherit
+}) => {
+  const {
+    expenses = 0, incomes = 0, progression = 0, today = 0,
+  } = currentMonth;
+  const progressionPercentage = currentBalance - progression > 0
+    ? (progression * 100) / (currentBalance - progression)
+    : progression;
 
-  static defaultProps = {
-    currency: CURRENCY,
-    currentBalance: undefined,
-    currentMonth: {},
-    mask: false,
-    onMask: undefined,
-    image: ASSETS.logo,
-  };
+  return (
+    <Consumer>
+      { ({ l10n, navigation, store: { baseCurrency, rates } }) => (
+        <View style={[styles.container, inherit.style]}>
+          <Box style={styles.card}>
+            <View style={[styles.row, styles.rowHeading]}>
+              <Image source={image} resizeMode="contain" style={styles.image} />
+              <Text caption level={2} style={styles.expand}>{title.toUpperCase()}</Text>
 
-  shouldComponentUpdate({ currency, currentBalance, mask }) {
-    const { props } = this;
+              <ButtonMore
+                title={l10n.ACTIVITY}
+                onPress={() => navigation.navigate(SCREEN.STATS)}
+              />
+            </View>
+            <Touchable onPress={onMask ? () => onMask(!mask) : undefined}>
+              <PriceFriendly
+                currency={baseCurrency}
+                headline
+                mask={mask}
+                level={4}
+                value={baseCurrency !== currency
+                  ? exchange(Math.abs(currentBalance), currency, baseCurrency, rates)
+                  : Math.abs(currentBalance)}
+              />
+            </Touchable>
+            { baseCurrency !== currency && (
+              <PriceFriendly
+                currency={currency}
+                mask={mask}
+                subtitle
+                level={3}
+                lighten
+                value={currentBalance}
+              />
+            )}
 
-    return currency !== props.currency || currentBalance !== props.currentBalance || mask !== props.mask;
-  }
-
-  render() {
-    const {
-      props: {
-        currency, currentBalance, currentMonth, image, mask, onMask, title, ...inherit
-      },
-    } = this;
-
-    const {
-      expenses = 0, incomes = 0, progression = 0, today = 0,
-    } = currentMonth;
-    const progressionPercentage = currentBalance - progression > 0
-      ? (progression * 100) / (currentBalance - progression)
-      : progression;
-
-    return (
-      <Consumer>
-        { ({ l10n, navigation, store: { baseCurrency, rates } }) => (
-          <View style={[styles.container, inherit.style]}>
-            <Box style={styles.card}>
-              <View style={[styles.row, styles.rowHeading]}>
-                <Image source={image} resizeMode="contain" style={styles.image} />
-                <Text caption level={2} style={styles.expand}>{title.toUpperCase()}</Text>
-
-                <ButtonMore
-                  title={l10n.ACTIVITY}
-                  onPress={() => navigation.navigate(SCREEN.STATS)}
-                />
+            <View style={styles.expand} />
+            <View style={styles.row}>
+              <View style={styles.rowItem}>
+                <Text {...captionProps}>{verboseMonth(new Date(), l10n).toUpperCase()}</Text>
+                <PriceSmall currency="%" icon mask={mask} value={progressionPercentage} />
               </View>
-              <Touchable onPress={onMask ? () => onMask(!mask) : undefined}>
-                <PriceFriendly
-                  currency={baseCurrency}
-                  headline
-                  mask={mask}
-                  level={4}
-                  value={baseCurrency !== currency
-                    ? exchange(Math.abs(currentBalance), currency, baseCurrency, rates)
-                    : Math.abs(currentBalance)}
-                />
-              </Touchable>
-              { baseCurrency !== currency && (
-                <PriceFriendly
-                  currency={currency}
-                  mask={mask}
-                  subtitle
-                  level={3}
-                  lighten
-                  value={currentBalance}
-                />
-              )}
 
-              <View style={styles.expand} />
-              <View style={styles.row}>
-                <View style={styles.rowItem}>
-                  <Text {...captionProps}>{verboseMonth(new Date(), l10n).toUpperCase()}</Text>
-                  <PriceSmall currency="%" icon mask={mask} value={progressionPercentage} />
-                </View>
-
-                <View style={styles.rowItem}>
-                  <Text {...captionProps}>{l10n.INCOMES.toUpperCase()}</Text>
-                  <PriceSmall currency={baseCurrency} mask={mask} value={incomes} />
-                </View>
-
-                <View style={[styles.rowItem, styles.rowItemExpanded]}>
-                  <Text {...captionProps}>{l10n.EXPENSES.toUpperCase()}</Text>
-                  <PriceSmall currency={baseCurrency} mask={mask} value={expenses} />
-                </View>
-
-                <View>
-                  <Text {...captionProps}>{l10n.TODAY.toUpperCase()}</Text>
-                  <PriceSmall currency={baseCurrency} mask={mask} value={today} />
-                </View>
+              <View style={styles.rowItem}>
+                <Text {...captionProps}>{l10n.INCOMES.toUpperCase()}</Text>
+                <PriceSmall currency={baseCurrency} mask={mask} value={incomes} />
               </View>
-            </Box>
-          </View>
-        )}
-      </Consumer>
-    );
-  }
-}
+
+              <View style={[styles.rowItem, styles.rowItemExpanded]}>
+                <Text {...captionProps}>{l10n.EXPENSES.toUpperCase()}</Text>
+                <PriceSmall currency={baseCurrency} mask={mask} value={expenses} />
+              </View>
+
+              <View>
+                <Text {...captionProps}>{l10n.TODAY.toUpperCase()}</Text>
+                <PriceSmall currency={baseCurrency} mask={mask} value={today} />
+              </View>
+            </View>
+          </Box>
+        </View>
+      )}
+    </Consumer>
+  );
+});
+
+Summary.propTypes = {
+  currency: string,
+  currentBalance: number,
+  currentMonth: shape({}),
+  image: oneOfType([number, string]),
+  mask: bool,
+  onMask: func,
+  title: string.isRequired,
+};
+
+Summary.defaultProps = {
+  currency: CURRENCY,
+  currentBalance: undefined,
+  currentMonth: {},
+  mask: false,
+  onMask: undefined,
+  image: ASSETS.logo,
+};
 
 export default Summary;
