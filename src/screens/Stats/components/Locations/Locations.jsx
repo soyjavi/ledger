bb2166499e13aa0d5c6d8f90e7a0003766f8c224
JΛVI <hello@soyjavi.com>
2020-02-28 12:28:52 +0700

    Some fixes

--- src/screens/Stats/components/Locations/Locations.jsx
+++ src/screens/Stats/components/Locations/Locations.jsx
@@ -19,7 +19,7 @@ const Locations = ({ cities, countries, points, precission }) => {
     <View style={styles.container}>
       <View style={styles.content}>
         <Heading value={l10n.LOCATIONS} style={styles.heading} />
-        <HeatMap color={COLOR.PRIMARY} points={points} precission={precission} />
+        <HeatMap color={COLOR.TEXT} points={points} precission={precission} />
       </View>
 
       <View style={styles.content}>
