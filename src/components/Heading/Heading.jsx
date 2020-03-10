7ce9ddb6a9a120fe23338d1ca8ce9b34b85aa034
JΛVI <hello@soyjavi.com>
2020-03-10 18:14:24 +0700

    <Heading> doesn't need <Header>

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,23 +1,14 @@
-import { node, number, oneOfType, string } from 'prop-types';
+import { node, string } from 'prop-types';
 import React from 'react';
-import { Image } from 'react-native';
 
 import { Col, Row, Text } from '../../reactor/components';
-import styles from './Heading.style';
 
-const Heading = ({ children, image, value, ...others }) => (
+const Heading = ({ children, value = '', ...others }) => (
   <Row marginBottom="XS" {...others}>
-    {image && (
-      <Col marginRight="S" width="auto">
-        <Image source={image} resizeMode="contain" style={styles.image} />
-      </Col>
-    )}
     <Col>
-      {value && (
-        <Text color={others.color} subtitle>
-          {value.toUpperCase()}
-        </Text>
-      )}
+      <Text color={others.color} subtitle>
+        {value.toUpperCase()}
+      </Text>
     </Col>
     <Col width="auto">{children}</Col>
   </Row>
@@ -25,7 +16,6 @@ const Heading = ({ children, image, value, ...others }) => (
 
 Heading.propTypes = {
   children: node,
-  image: oneOfType([number, string]),
   value: string,
 };
 
