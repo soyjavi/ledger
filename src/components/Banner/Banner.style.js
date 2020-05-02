b5cbb240ba75dfe587e0cf437923043a004d08a1
JΛVI <hello@soyjavi.com>
2020-05-02 13:02:49 +0700

    Improved <Banner>

--- src/components/Banner/Banner.style.js
+++ src/components/Banner/Banner.style.js
@@ -1,8 +1,10 @@
 import { StyleSheet } from 'react-native';
-import { LAYOUT } from 'reactor/common';
+import { LAYOUT, THEME } from 'reactor/common';
+
+const { SPACE } = THEME;
 
 const {
-  VIEWPORT: { H },
+  VIEWPORT: { H, W },
 } = LAYOUT;
 
 let IMAGE_HEIGHT = H / 4;
@@ -15,26 +17,36 @@ export default StyleSheet.create({
 
   image: {
     height: IMAGE_HEIGHT,
-    width: '100%',
+    width: '75%',
   },
 
   left: {
-    alignItems: 'left',
+    alignItems: 'flex-start',
   },
 
   right: {
-    alignItems: 'right',
+    alignItems: 'flex-end',
+    alignContent: 'flex-end',
   },
 
   text: {
     textAlign: 'center',
+    // maxWidth: '75%',
+    // width: '100%',
   },
 
   textleft: {
     textAlign: 'left',
+    marginRight: SPACE.XXL + SPACE.M,
   },
 
   textright: {
     textAlign: 'right',
+    marginLeft: SPACE.XXL + SPACE.M,
+  },
+
+  caption: {
+    fontSize: 14,
+    lineHeight: 14 * 1.5,
   },
 });
