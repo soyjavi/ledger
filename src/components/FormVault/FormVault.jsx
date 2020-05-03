0b7b0242aa038bce5c311aa2b1b0a84d307cb1eb
JΛVI <hello@soyjavi.com>
2020-05-03 10:16:56 +0700

    <FormVault> is a common component

---
+++ src/components/FormVault/FormVault.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { THEME } from 'reactor/common';
+import { Slider } from 'reactor/components';
+
+import { FLAGS } from '@assets';
+import { useL10N, useStore } from '@context';
+import { Option, OPTION_SIZE } from '@components/Option';
+import { Input } from '@components/Input';
+
+import { queryCurrencies } from './modules';
+
+const { SPACE } = THEME;
+
+export const FormVault = ({ form = {}, onChange }) => {
+  const l10n = useL10N();
+  const store = useStore();
+
+  const handleField = (field, value) => {
+    const next = { ...form, [field]: value };
+
+    onChange({
+      ...next,
+      valid: next.currency !== undefined && next.title !== undefined,
+    });
+  };
+
+  return (
+    <>
+      <Slider itemMargin={SPACE.S} itemWidth={OPTION_SIZE}>
+        {queryCurrencies(store).map((currency, index) => (
+          <Option
+            caption={currency}
+            image={FLAGS[currency]}
+            key={index}
+            marginRight="S"
+            onPress={() => handleField('currency', currency)}
+            selected={form.currency === currency}
+          />
+        ))}
+      </Slider>
+      <Input
+        currency={form.currency}
+        label={l10n.INITIAL_BALANCE}
+        marginVertical="M"
+        onChange={(value) => handleField('balance', value)}
+        value={form.balance}
+      />
+      <Input label={l10n.ACCOUNT_NAME} onChange={(value) => handleField('title', value)} value={form.title} />
+    </>
+  );
+};
+
+FormVault.propTypes = {
+  form: PropTypes.shape({}),
+  onChange: PropTypes.func.isRequired,
+};
---
+++ src/components/FormVault/modules/queryCurrencies.js
@@ -0,0 +1,9 @@
+import { C } from '@common';
+
+const { CURRENCY } = C;
+
+export const queryCurrencies = ({ baseCurrency = CURRENCY, rates = {} }) => {
+  const keys = Object.keys(rates);
+
+  return keys.length > 0 ? [...new Set([baseCurrency, ...Object.keys(rates[keys[keys.length - 1]])])] : [];
+};
