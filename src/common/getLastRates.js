92b27cddccc42a575159368096141235baa50f75
JΛVI <hello@soyjavi.com>
2020-08-11 10:27:14 +0700

    Fixed type

--- src/common/getLastRates.js
+++ src/common/getLastRates.js
@@ -1,5 +1,5 @@
-export const getLastRates = (rates = []) => {
-  if (rates.length === 0) return rates;
+export const getLastRates = (rates = {}) => {
+  if (Object.keys(rates).length === 0) return rates || {};
 
   const keys = Object.keys(rates);
 
