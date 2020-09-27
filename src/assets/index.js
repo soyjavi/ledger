96150e488c1f2befffeabed6981f8d8ebe501d2e
JΛVI <hello@soyjavi.com>
2020-09-27 13:33:01 +0700

    We dont need the logo within the App

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,5 @@
-import LOGO from './logo.png';
 import NOT_FOUND from './notfound.png';
 import RECEIVE from './receive.png';
 import SEND from './send.png';
 
-const BANNERS = {
-  NOT_FOUND,
-  RECEIVE,
-  SEND,
-};
-
-export { BANNERS, LOGO };
+export const BANNERS = { NOT_FOUND, RECEIVE, SEND };
