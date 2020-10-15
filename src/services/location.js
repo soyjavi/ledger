5b2335e3f0523d930d08a3446427c3ffc1103c98
JΛVI <hello@soyjavi.com>
2020-10-15 18:09:07 +0700

    Using mapbox service directly

--- src/services/location.js
+++ src/services/location.js
@@ -4,21 +4,40 @@ import { objectToQueryString } from './modules';
 
 const { MAPBOX, ENDPOINT } = C;
 
-const PARAMETERS = 'language=en&limit=1&types=place';
-
 export const ServiceLocation = {
   getPlace: async ({ latitude, longitude }) => {
+    const PARAMETERS = 'language=en&limit=10&types=poi';
     const url = `${MAPBOX.URL}/${longitude},${latitude}.json?${PARAMETERS}&access_token=${MAPBOX.ACCESS_TOKEN}`;
-    console.log({ url });
     const response = await fetch(url);
-    let place;
+    let POIs;
 
     if (response) {
       const { features = [] } = (await response.json()) || {};
-      place = features[0] ? features[0].place_name_en : 'Unknown Place';
+
+      POIs = features.map(({ id, properties: { address }, text_en: text, context = [] }) => {
+        const info = {};
+        context.forEach(({ id, text_en: value }) => {
+          const [key] = id.split('.');
+          if (['postcode', 'place', 'region', 'country'].includes(key)) info[key] = value;
+        });
+        const { place: city, ...location } = info;
+
+        return {
+          address,
+          id,
+          text,
+          ...location,
+          city,
+        };
+      });
+
+      // @TODO: Should offer options and user will choose one of them
+      const [{ city = '', region = '', country = '' }] = POIs;
+
+      return `${city}, ${region}, ${country}`;
     }
 
-    return place;
+    return;
   },
 
   uriMap: ({ color, darkMode = false, height = 0, points, precission, small, width = 0 }) => {
