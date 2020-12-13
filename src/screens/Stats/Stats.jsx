4ca1617d58f9b3b429effad46e3dcdaf8f39b117
JΛVI <hello@soyjavi.com>
2020-12-13 10:17:18 +0700

    Improved queryMonth() within screen <Stats>

--- src/screens/Stats/Stats.jsx
+++ src/screens/Stats/Stats.jsx
@@ -3,9 +3,8 @@ import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { THEME } from 'reactor/common';
 import { Viewport } from 'reactor/components';
 
-import { BANNERS } from '@assets';
 import { C } from '@common';
-import { Banner, Chart, Header, ScrollView, Summary } from '@components';
+import { Chart, Header, ScrollView, Summary } from '@components';
 import { useL10N, useNavigation, useStore } from '@context';
 
 import { ItemGroupCategories, Locations, SliderMonths } from './components';
@@ -28,7 +27,7 @@ const Stats = ({ visible, ...inherit }) => {
 
   const [chart, setChart] = useState({});
   const [slider, setSlider] = useState({});
-  const [month, setMonth] = useState({});
+  const [month, setMonth] = useState(undefined);
   const [scroll, setScroll] = useState(false);
 
   const {
@@ -45,14 +44,15 @@ const Stats = ({ visible, ...inherit }) => {
   }, [visible]);
 
   useEffect(() => {
-    if (visible) setMonth(queryMonth(store, slider));
-  }, [slider, store, visible]);
+    if (visible && Object.keys(slider).length > 0) setMonth(queryMonth(store, slider));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [visible, slider]);
 
   const handleSliderChange = (next) => {
     if (next.index !== slider.index) setSlider(next);
   };
 
-  const { expenses = {}, incomes = {}, locations = {} } = month;
+  const { expenses = {}, incomes = {}, locations = {} } = month || {};
   const hasExpenses = Object.keys(expenses).length > 0;
   const hasIncomes = Object.keys(incomes).length > 0;
   const hasPoints = locations.points && locations.points.length > 0;
@@ -98,28 +98,22 @@ const Stats = ({ visible, ...inherit }) => {
           values={chart.expenses}
         />
 
-        {hasExpenses || hasIncomes ? (
+        {(hasExpenses || hasIncomes || hasPoints) && (
           <>
             {hasIncomes && <ItemGroupCategories color={COLOR.BRAND} type={INCOME} dataSource={incomes} />}
             {hasExpenses && <ItemGroupCategories type={EXPENSE} dataSource={expenses} />}
+            {hasPoints && <Locations {...locations} />}
           </>
-        ) : (
-          <Banner image={BANNERS.NOT_FOUND} paddingHorizontal="M" paddingVertical="M" title={l10n.NO_TRANSACTIONS} />
         )}
 
-        {(hasExpenses || hasIncomes) && (
-          <>
-            {hasPoints && <Locations {...locations} />}
-            <Chart
-              {...calcScales(chart.transfers)}
-              {...chartProps}
-              captions={orderCaptions(l10n)}
-              styleContainer={[styles.chart, styles.chartMargin]}
-              title={l10n.TRANSFERS}
-              values={chart.transfers}
-            />
-          </>
-        )}
+        <Chart
+          {...useMemo(() => calcScales(chart.transfers), [chart.transfers])}
+          {...chartProps}
+          captions={orderCaptions(l10n)}
+          styleContainer={[styles.chart, styles.chartMargin]}
+          title={l10n.TRANSFERS}
+          values={chart.transfers}
+        />
       </ScrollView>
     </Viewport>
   );
