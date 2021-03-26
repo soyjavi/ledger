2d24adee44a9f12300668700e51db88d0feb3a9d
JΛVI <hello@soyjavi.com>
2021-03-26 20:11:05 +0700

    New style for <Footer>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,27 +3,53 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { THEME } from 'reactor/common';
 import { Motion, View } from 'reactor/components';
+import { Icon, Text, Touchable } from 'reactor/components';
+
+import { C } from '@common';
+import { useL10N, useNavigation } from '@context';
 
 import styles from './Footer.style';
 
-const { BLUR, MOTION, UNIT } = THEME;
+const { SCREEN } = C;
+const { BLUR, COLOR, ICON, MOTION, UNIT } = THEME;
 
-const Footer = ({ children, visible }) => (
-  <Motion
-    delay={MOTION.COLLAPSE}
-    duration={visible ? MOTION.EXPAND : MOTION.COLLAPSE}
-    pointerEvents="auto"
-    style={styles.container}
-    timeline={[{ property: 'translateY', value: visible ? 0 : UNIT * 16 }]}
-  >
-    <BlurView {...BLUR} style={styles.blur}>
-      <View style={styles.content}>{children}</View>
-    </BlurView>
-  </Motion>
-);
+const Footer = ({ visible = true }) => {
+  const l10n = useL10N();
+  const navigation = useNavigation();
+
+  const OPTIONS = [
+    //
+    { icon: 'home', text: l10n.DASHBOARD, section: SCREEN.DASHBOARD },
+    { icon: 'chart', text: l10n.ACTIVITY, section: SCREEN.STATS },
+    { icon: 'list', text: l10n.VAULTS, section: SCREEN.VAULTS },
+    { icon: 'settings', text: l10n.SETTINGS, section: SCREEN.SETTINGS },
+  ];
+
+  return (
+    <Motion
+      delay={MOTION.COLLAPSE}
+      duration={visible ? MOTION.EXPAND : MOTION.COLLAPSE}
+      pointerEvents="auto"
+      style={styles.container}
+      timeline={[{ property: 'translateY', value: visible ? 0 : UNIT * 16 }]}
+    >
+      <BlurView {...BLUR} style={styles.blur}>
+        <View style={styles.content}>
+          {OPTIONS.map(({ icon, text, section }, index) => (
+            <Touchable key={index} style={styles.option} onPress={() => navigation.go(section)}>
+              <Icon color={index === 0 ? COLOR.BRAND : COLOR.LIGHTEN} value={icon} family={ICON.FAMILY} />
+              <Text bold={index === 0} caption color={index === 0 ? COLOR.TEXT : COLOR.LIGHTEN} marginTop="S">
+                {text}
+              </Text>
+            </Touchable>
+          ))}
+        </View>
+      </BlurView>
+    </Motion>
+  );
+};
 
 Footer.propTypes = {
-  children: PropTypes.node,
   visible: PropTypes.bool,
 };
 
