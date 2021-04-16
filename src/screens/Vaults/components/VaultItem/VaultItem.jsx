cf438bbd14b61dcfd666ce6e6f0998908421e91f
JΛVI <hello@soyjavi.com>
2021-04-16 16:28:45 +0700

    Improved <VaultItem> when no balance

--- src/screens/Vaults/components/VaultItem/VaultItem.jsx
+++ src/screens/Vaults/components/VaultItem/VaultItem.jsx
@@ -15,6 +15,8 @@ const VaultItem = ({ onPress, dataSource: { currency, currentBalance, title } })
     settings: { baseCurrency },
   } = useStore();
 
+  const noBalance = currentBalance === 0;
+
   return (
     <Touchable
       paddingHorizontal="M"
@@ -25,12 +27,18 @@ const VaultItem = ({ onPress, dataSource: { currency, currentBalance, title } })
     >
       <Row>
         <Col marginRight="S" width="auto">
-          <Box styleContent={styles.boxContent}>
-            <CurrencyLogo color={currency !== baseCurrency ? COLOR.LIGHTEN : undefined} currency={currency} size="S" />
+          <Box outlined={noBalance} styleContent={styles.boxContent}>
+            <CurrencyLogo
+              color={currency !== baseCurrency || noBalance ? COLOR.LIGHTEN : undefined}
+              currency={currency}
+              size="S"
+            />
           </Box>
         </Col>
         <Col>
-          <Text numberOfLines={1}>{title}</Text>
+          <Text color={noBalance ? COLOR.LIGHTEN : undefined} numberOfLines={1}>
+            {title}
+          </Text>
           <PriceFriendly caption color={COLOR.LIGHTEN} currency={currency} value={currentBalance} />
         </Col>
       </Row>
