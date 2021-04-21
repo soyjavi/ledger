0755f6bc37803f954b1c356ea7d865c5b29935a4
JΛVI <hello@soyjavi.com>
2021-04-21 15:24:03 +0700

    Simplify <CurrencyLogo>

--- src/components/CurrencyLogo/CurrencyLogo.style.js
+++ src/components/CurrencyLogo/CurrencyLogo.style.js
@@ -3,54 +3,24 @@ import { THEME } from 'reactor/common';
 
 const { COLOR, SPACE } = THEME;
 
-const SIZE = {
-  S: SPACE.M,
-  M: SPACE.L,
-  L: SPACE.XXL,
-};
+const SIZE = SPACE.M + SPACE.XS;
 
 export default StyleSheet.create({
   container: {
     backgroundColor: COLOR.TEXT,
-    borderRadius: SIZE.M,
+    borderRadius: SIZE,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    height: SIZE.M,
-    width: SIZE.M,
-  },
-
-  S: {
-    borderRadius: SIZE.S,
-    height: SIZE.S,
-    width: SIZE.S,
-  },
-
-  L: {
-    borderRadius: SIZE.L,
-    height: SIZE.L,
-    width: SIZE.L,
+    height: SIZE,
+    width: SIZE,
   },
 
   font: {
-    height: SIZE.M,
-    lineHeight: SIZE.M * 1.05,
+    height: SIZE,
+    lineHeight: SIZE * 1.05,
     overflow: 'hidden',
     textAlign: 'center',
-    width: SIZE.M,
-  },
-
-  fontS: {
-    fontSize: SIZE.S * 0.6,
-    height: SIZE.S,
-    lineHeight: SIZE.S * 1.05,
-    width: SIZE.S,
-  },
-
-  fontL: {
-    fontSize: SIZE.L * 0.6,
-    height: SIZE.L,
-    lineHeight: SIZE.L * 1.05,
-    width: SIZE.L,
+    width: SIZE,
   },
 });
