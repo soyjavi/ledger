9b2617501b04639c6a3fc049cc12ee69e56613e3
JΛVI <hello@soyjavi.com>
2021-04-21 09:58:35 +0700

    New way for sync

--- src/App.sync.jsx
+++ src/App.sync.jsx
@@ -1,16 +1,14 @@
 import React, { useCallback, useEffect, useLayoutEffect, useState } from 'react';
-import { SafeAreaView } from 'react-native';
 import { THEME } from 'reactor/common';
-import { Alert, Motion, Snackbar, Text } from 'reactor/components';
+import { Alert } from 'reactor/components';
 
 import { C } from '@common';
 import { useConnection, useL10N, useSnackBar, useStore } from '@context';
 
-import styles from './App.style';
 import { getRates, getSyncStatus, syncNode } from './App.sync.controller';
 
 const { DELAY_PRESS_MS, TIMEOUT } = C;
-const { COLOR, MOTION, SPACE } = THEME;
+const { COLOR } = THEME;
 
 const STATE = { UNKNOWN: 0, FETCHING: 1, UNSYNCED: 2, SYNCING: 3, SYNCED: 4 };
 
@@ -44,6 +42,11 @@ const Sync = () => {
     if (!connected || [STATE.UNKNOWN, STATE.FETCHING, STATE.SYNCING].includes(state)) clearTimeout(syncTimeout);
     else syncTimeout = setTimeout(handleState, TIMEOUT.SYNC);
 
+    if (connected) {
+      if (state === STATE.SYNCING) snackbar.info(l10n.SYNC_BUSY, false);
+      else if (state !== STATE.SYNCED) snackbar.close();
+    }
+
     return () => clearTimeout(syncTimeout);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [connected, state, store]);
@@ -52,51 +55,35 @@ const Sync = () => {
     setState(STATE.FETCHING);
     const synced = await getSyncStatus({ setState, snackbar, STATE, store });
 
-    if (synced === undefined) setState(STATE.UNKNOWN);
+    if (synced === undefined && state !== STATE.UNKNOWN) setState(STATE.UNKNOWN);
     else setState(synced ? STATE.SYNCED : STATE.UNSYNCED);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state, store]);
 
   const handleSync = async () => {
     setState(STATE.SYNCING);
+
     const synced = await syncNode({ store, snackbar });
     setState(synced ? STATE.SYNCED : STATE.UNSYNCED);
+
     if (synced) snackbar.success(l10n.SYNC_DONE);
   };
 
   return (
-    <>
-      <SafeAreaView>
-        <Motion
-          duration={!connected ? MOTION.EXPAND : MOTION.COLLAPSE}
-          style={styles.connected}
-          timeline={[{ property: 'translateY', value: !connected ? 0 : -(SPACE.XXL * 4) }]}
-          type="spring"
-        >
-          <Text bold caption>
-            {l10n.OFFLINE}
-          </Text>
-        </Motion>
-      </SafeAreaView>
-
-      {connected && (
-        <>
-          <Alert
-            accept={l10n.SYNC_NOW}
-            cancel={l10n.LATER}
-            caption={l10n.SYNC_CAPTION}
-            delay={DELAY_PRESS_MS}
-            position="bottom"
-            title={l10n.WARNING}
-            visible={state === STATE.UNSYNCED}
-            onAccept={handleSync}
-            onCancel={() => setState(undefined)}
-          />
-
-          <Snackbar caption={l10n.SYNC_BUSY} color={COLOR.BASE} position="top" visible={STATE.SYNCING === state} />
-        </>
-      )}
-    </>
+    connected && (
+      <Alert
+        accept={l10n.SYNC_NOW}
+        cancel={l10n.LATER}
+        caption={l10n.SYNC_CAPTION}
+        delay={DELAY_PRESS_MS}
+        position="bottom"
+        style={{ backgroundColor: COLOR.ERROR }}
+        title={l10n.WARNING}
+        visible={state === STATE.UNSYNCED}
+        onAccept={handleSync}
+        onCancel={() => setState(undefined)}
+      />
+    )
   );
 };
 
