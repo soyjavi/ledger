9bb9965efb4c60764c092a737a84747fcf88f1c9
JΛVI <hello@soyjavi.com>
2021-07-04 18:08:36 +0700

    First version of <Input> using @lookiero/aurora

--- src/components/Input/Input.style.js
+++ src/components/Input/Input.style.js
@@ -1,43 +1,37 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from 'reactor/common';
+import StyleSheet from 'react-native-extended-stylesheet';
+import { Platform } from 'react-native';
 
-const { BORDER_RADIUS, COLOR, FONT, SPACE } = THEME;
-
-const CONTAINER_HEIGHT = SPACE.XXL + SPACE.L;
-
-export default StyleSheet.create({
+export const style = StyleSheet.create({
   container: {
-    width: '100%',
-    backgroundColor: COLOR.BASE,
-    borderColor: COLOR.TRANSPARENT,
-    borderRadius: BORDER_RADIUS,
-    borderWidth: 1,
-    height: CONTAINER_HEIGHT,
-    minHeight: CONTAINER_HEIGHT,
-  },
-
-  active: {
-    borderColor: COLOR.TEXT,
-  },
-
-  fulfilled: {
-    backgroundColor: COLOR.TRANSPARENT,
+    borderStyle: '$borderStyle',
+    borderWidth: '$borderSize',
+    paddingTop: '$spaceM',
   },
 
   input: {
-    ...FONT.BOLD,
-    ...FONT.INPUT,
-    color: COLOR.TEXT,
-    height: '100%',
-    paddingHorizontal: SPACE.M,
-    paddingTop: SPACE.M + (SPACE.XS - SPACE.XS / 2),
+    color: '$colorContent',
+    flex: 0,
+    fontFamily: '$fontInput',
+    fontSize: '$fontInputSize',
+    height: '$inputSize',
     margin: 0,
+    minHeight: '$inputSize',
+    paddingTop: '$fontInputPaddingTop',
+    paddingRight: '$fontInputPaddingRight',
+    paddingBottom: '$fontInputPaddingBottom',
+    paddingLeft: '$fontInputPaddingLeft',
+    textAlignVertical: 'center',
+    ...Platform.select({
+      web: {
+        outlineWidth: 0,
+      },
+    }),
     width: '100%',
   },
 
   label: {
     position: 'absolute',
-    top: SPACE.M,
-    left: SPACE.M,
+    top: '$spaceM',
+    left: '$spaceM',
   },
 });
