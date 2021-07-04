2d6135c5055f84128eccdf9b1188ee66b179f7c2
JΛVI <hello@soyjavi.com>
2021-07-04 08:58:17 +0700

    Screen <Welcome> ported to @lookiero/aurora

--- src/screens/Onboarding/components/Welcome/Welcome.jsx
+++ src/screens/Onboarding/components/Welcome/Welcome.jsx
@@ -1,31 +1,43 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Button, Viewport } from 'reactor/components';
+import {
+  // helpers
+  ALIGN,
+  SIZE as SPACE,
+  LAYOUT,
+  // components
+  Button,
+  SafeAreaView,
+  View,
+} from '@lookiero/aurora';
 
 import { BANNERS } from '@assets';
 import { C } from '@common';
-import { Banner } from '@components';
+import { Banner, Viewport } from '@components';
 import { useL10N } from '@context';
 
-import styles from './Welcome.style';
-
-const { DELAY_PRESS_MS } = C;
+import { style } from './Welcome.style';
 
 const Welcome = ({ onPress, ...inherit }) => {
   const l10n = useL10N();
 
   return (
     <Viewport {...inherit}>
-      <SafeAreaView>
-        <Banner
-          align="left"
-          image={BANNERS.WELCOME}
-          marginBottom="XL"
-          title={l10n.WELCOME_TITLE}
-          caption={l10n.WELCOME_CAPTION}
-        />
-        <Button delay={DELAY_PRESS_MS} onPress={onPress} style={styles.button} text={l10n.GET_STARTED} />
+      <SafeAreaView flex={SPACE.XS}>
+        <View customStyle={style.content} justifyContent={ALIGN.END} padding={SPACE.M}>
+          <Banner
+            aling={ALIGN.LEFT}
+            image={BANNERS.WELCOME}
+            marginBottom={LAYOUT.XS}
+            title={l10n.WELCOME_TITLE}
+            caption={l10n.WELCOME_CAPTION}
+          />
+          <View alignSelf={ALIGN.START}>
+            <Button wide={false} onPress={onPress}>
+              {l10n.GET_STARTED}
+            </Button>
+          </View>
+        </View>
       </SafeAreaView>
     </Viewport>
   );
--- src/screens/Onboarding/components/Welcome/Welcome.style.js
+++ src/screens/Onboarding/components/Welcome/Welcome.style.js
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native';
+import StyleSheet from 'react-native-extended-stylesheet';
 
-export default StyleSheet.create({
-  button: {
-    alignSelf: 'flex-start',
+export const style = StyleSheet.create({
+  content: {
+    height: '100%',
   },
 });
