d1f538e2dda2b2daafe7589520981d100ba9aa44
JΛVI <hello@soyjavi.com>
2021-07-29 10:35:06 +0700

    Show base currency in chart headings

--- src/screens/Stats/components/Chart/Chart.Heading.jsx
+++ src/screens/Stats/components/Chart/Chart.Heading.jsx
@@ -5,15 +5,16 @@ import { Heading, PriceFriendly } from '@components';
 
 import { style } from './Chart.style';
 
-const ChartHeading = ({ color, inverted, max, min, title }) => (
+const ChartHeading = ({ color, currency, inverted, max, min, title }) => (
   <Heading style={inverted && style.headingInverted} value={title}>
-    {min > 0 && <PriceFriendly color={color} detail label="min " value={min} />}
-    {max > 0 && <PriceFriendly color={color} detail label="  max " value={max} />}
+    {min > 0 && <PriceFriendly color={color} currency={currency} detail label="min " value={min} />}
+    {max > 0 && <PriceFriendly color={color} currency={currency} detail label="  max " value={max} />}
   </Heading>
 );
 
 ChartHeading.propTypes = {
   color: PropTypes.string,
+  currency: PropTypes.string,
   inverted: PropTypes.bool,
   max: PropTypes.number,
   min: PropTypes.number,
