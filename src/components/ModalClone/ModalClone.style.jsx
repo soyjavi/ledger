613fbab2dc2a10fd20243895af2505d72389d734
JΛVI <hello@soyjavi.com>
2021-10-17 17:31:21 +0700

    Padding bottom in all modals

--- src/components/ModalClone/ModalClone.style.jsx
+++ src/components/ModalClone/ModalClone.style.jsx
@@ -2,6 +2,7 @@ import StyleSheet from 'react-native-extended-stylesheet';
 
 export const style = StyleSheet.create({
   modalContent: {
+    paddingBottom: '$spaceM',
     paddingHorizontal: '$spaceM',
   },
 });
--- src/components/ModalTransaction/ModalTransaction.style.jsx
+++ src/components/ModalTransaction/ModalTransaction.style.jsx
@@ -3,6 +3,7 @@ import StyleSheet from 'react-native-extended-stylesheet';
 export const style = StyleSheet.create({
   modalContent: {
     overflow: 'hidden',
+    paddingBottom: '$spaceM',
     paddingHorizontal: '$spaceM',
   },
 });
