ce93cc250e774682a57f7043a205d42000021d6a
JΛVI <hello@soyjavi.com>
2021-12-17 08:59:01 +0700

    Improved consolidate

--- src/context/modules/consolidate.js
+++ src/context/modules/consolidate.js
@@ -9,22 +9,30 @@ export const consolidate = ({
   txs: baseTxs = [],
   vaults: baseVaults = [],
 } = {}) => {
-  let txs = baseTxs.map(({ data = {}, hash, timestamp }) => ({ timestamp, ...data, hash }));
+  let txs = baseTxs.map(({ hash, timestamp, data = {}, ...others }) => ({
+    hash,
+    timestamp,
+    ...others,
+    ...data,
+  }));
+
   let vaults = [];
 
-  if (baseVaults.length > 1) {
-    const { timestamp: blockTimestamp, data: { timestamp } = {} } = baseVaults[1];
+  if (baseVaults.length > 0) {
+    const { timestamp: blockTimestamp, data: { timestamp } = {} } = baseVaults[0].balance
+      ? baseVaults[0]
+      : baseVaults[1];
     const genesisDate = new Date(timestamp || blockTimestamp);
     const months = getMonthDiff(genesisDate, new Date());
 
-    vaults = baseVaults.map(({ data = {}, hash, timestamp }) =>
+    vaults = baseVaults.map(({ hash, timestamp, data = {}, ...others }) =>
       calcVault({
         baseCurrency,
         genesisDate,
         months,
         rates,
         txs,
-        vault: { timestamp, ...data, hash },
+        vault: { hash, timestamp, ...data, ...others },
       }),
     );
   }
