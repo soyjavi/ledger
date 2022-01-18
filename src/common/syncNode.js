42518626e78601ce875de7ced5833402c7d3733f
JΛVI <hello@soyjavi.com>
2022-01-18 09:28:33 +0700

    Simplify sync

--- src/common/syncNode.js
+++ src/common/syncNode.js
@@ -6,33 +6,19 @@ const findHashIndex = (blockchain = [], latestHash) => blockchain.findIndex(({ h
 
 const blocksToSync = (blockchain = [], latestHash) => blockchain.slice(findHashIndex(blockchain, latestHash) + 1);
 
-const parseBlocks = (values = [], field) =>
-  values.map(({ data = {}, timestamp, ...others }) => ({
-    ...others,
-    timestamp: new Date(timestamp).getTime(),
-    data:
-      typeof data === 'string' ? data : { ...data, [field]: data[field] === null || !data[field] ? 0 : data[field] },
-  }));
-
 // ! @TODO: use new system
 export const syncNode = async (store) => {
-  const { blockchain, settings } = store;
-  const txs = blockchain.get('txs').blocks;
-  const vaults = blockchain.get('vaults').blocks;
+  const { settings, txs, vaults } = store;
   const { txs: nodeTxs = {}, vaults: nodeVaults = {} } = (await ServiceNode.status({ settings })) || {};
 
   const vaultsToSync = blocksToSync(vaults, nodeVaults.latestHash);
   const txsToSync = blocksToSync(txs, nodeTxs.latestHash);
 
   if (vaultsToSync.length > 0 && txsToSync.length > 0) {
-    await ServiceNode.sync({
-      settings,
-      blockchain: { vaults: parseBlocks(vaults, 'balance'), txs: parseBlocks(txs, 'value') },
-    });
+    await ServiceNode.sync({ settings, blockchain: { vaults, txs } });
   } else {
-    if (vaultsToSync.length > 0)
-      await ServiceNode.sync({ settings, key: 'vaults', blocks: parseBlocks(vaultsToSync, 'balance') });
-    if (txsToSync.length > 0) await ServiceNode.sync({ settings, key: 'txs', blocks: parseBlocks(txsToSync, 'value') });
+    if (vaultsToSync.length > 0) await ServiceNode.sync({ settings, key: 'vaults', blocks: vaultsToSync });
+    if (txsToSync.length > 0) await ServiceNode.sync({ settings, key: 'txs', blocks: txsToSync });
   }
 
   return await getSyncStatus(store);
