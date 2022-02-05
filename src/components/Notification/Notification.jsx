5b46f34c58690816ad8f9f7151b6e05001eb78ba
JΛVI <hello@soyjavi.com>
2022-02-05 18:42:31 +0700

    Improving Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,13 +1,27 @@
-import { Notification as AuroraNotification, Text } from '@lookiero/aurora';
+import {
+  // helpers
+  ALIGN,
+  FLEX_DIRECTION,
+  // components
+  Icon,
+  Notification as AuroraNotification,
+  Text,
+  View,
+} from '@lookiero/aurora';
 import PropTypes from 'prop-types';
 import React from 'react';
+
+import { style } from './Notification.style';
 
 const Notification = ({ text, ...others }) => {
   return (
     <AuroraNotification {...others}>
-      <Text detail level={1}>
-        {text}
-      </Text>
+      <View alignItems={ALIGN.CENTER} flexDirection={FLEX_DIRECTION.ROW}>
+        <Icon name="info" style={style.icon} />
+        <Text detail level={1}>
+          {text}
+        </Text>
+      </View>
     </AuroraNotification>
   );
 };
--- src/components/Notification/Notification.style.js
+++ src/components/Notification/Notification.style.js
@@ -1,13 +1,11 @@
 import StyleSheet from 'react-native-extended-stylesheet';
 
 export const style = StyleSheet.create({
-  notification: {
-    width: 'auto',
-  },
-
-  button: {
-    marginLeft: '$spaceS',
-    marginTop: '$spaceS * -1',
-    marginRight: '$spaceS * -1',
+  icon: {
+    fontSize: '$spaceXL',
+    height: '$spaceXL',
+    marginRight: '$spaceS',
+    minWidth: '$spaceXL',
+    width: '$spaceXL',
   },
 });
